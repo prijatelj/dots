60712e7696c421a4ebac1283f606fd05d613babb
prijatelj <theomnipotentwoof@gmail.com>
2020-06-17 08:24:08 -0400

    added todos

--- random_password.sh
+++ random_password.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 # creates a random string, good for passwords.
 # TODO save output into clipboard
+# TODO set optinal argument for password length
+# TODO set optinal argument for regex of valid characters
 LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 64 ; echo
