f7cf41e339578f9db8cf68bc2e6b6430c73b842d
prijatelj <theomnipotentwoof@gmail.com>
2021-08-03 16:24:24 -0400

    added more details

--- .config/rice/i3/firefox_restore.sh
+++ .config/rice/i3/firefox_restore.sh
@@ -7,11 +7,31 @@
 #   not firefox specific. We want to be able to reload the prior workspace
 #   setup in genral.
 
+## TODO make sure you do a check of FOSS code for this, it may already exist...
+## it should, tbh
+
+
 # TODO Open firefox in one workspace
+firefox
 
 # TODO If there is a prior firefox workspace setup and firefox reopened the
 # windows that corresponded to that prior setup, then moves those firefox
 # windows to their correct workspace and orientation within that workspace.
+#   TODO orientation of windows in a workspace
+#   TODO size of windows in a workspace
+
+# TODO load from some file the workspace setup
+
+# TODO for each open firefox window, match its id to the id in workspace setup
+#  TODO Move that window to its proper place, starting with top level of that
+#  workspace. Code module for one workspace at a time, optionally parallelize.
+
+# TODO Once that level of the workspace is occupied with the correct firefox
+# windows, resize them as appropriate.
+
+# TODO If there is more children in i3wm window tree, continue from top-down
+
+
 
 # TODO upon termination of firefox, either by quitting, logging out, shutting
 # down, power loss, or otherwise ending the current xorg session when firefox
@@ -19,4 +39,5 @@
 # such that it may be recovered.
 #   TODO Include a script or daemon that runs unprivileged for the user-session
 #   and updates the current workspace setup, either by a timer e.g. every hour,
-#   or if a change has occurred, or manually via a command run by the user.
+#   or if a change has occurred in workspace, or manually via a command run by
+#   the user.
