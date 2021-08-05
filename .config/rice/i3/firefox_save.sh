3bb24faaa06abbd163e79fcd0114853534d3802d
prijatelj <theomnipotentwoof@gmail.com>
2021-08-05 17:01:32 -0400

    WIP: simplified

--- .config/rice/i3/firefox_save.sh
+++ .config/rice/i3/firefox_save.sh
@@ -8,36 +8,16 @@
 # TODO If layout change persists for longer than the given amount time, update
 # saved layouts
 
-# Get all monitor outputs as bash array, diregarding the first which is x-root
-MONITORS=($(i3-msg -t get_outputs | jq -r '.[1:] | map(.name)' | tr -d '[]," '))
+# Loop through the workspaces, saving their layouts if they contain firefox.
+for workspace in $(echo $(i3-msg -t get_workspaces) | jq -c '.[]'); do
+    output_name=($(echo $workspace | jq -r '[.output, .name]' | tr -d '[]," '))
 
-# TODO get the workspaces per monitor
-WORKSPACES=$(i3-msg -t get_workspaces)
-
-MONITORS=($(echo $WORKSPACES | jq -r '[to_entries[] | .value.output] | unique' | tr -d '[]," '))
-
-# TODO use the tree to find the workspaces' outputs
-
-# Get the json of Outputs and their respective workspaces
-for OUTPUT in "${MONITORS[@]}"; do
-    # TODO Loop through each workspace per monitor
-
-    # Obtain the json of that output, rm unnecessary comments, uncomment remainder
-    for WORKSPACE in "${WORKSPACES[@]}"; do
-        i3-save-tree --workspace $WORKSPACE | tail -n +2 | head -n -1 \
-            | fgrep -v -e '// split' -e '// workspace' | sed 's|// ||g' \
-            | jq \ # rm undesirable keys in swallows
-            > "$FIREFOX_LAYOUT_PREFIX"_md-"$OUTPUT"_ws-"$WORKSPACE".json
-    done
+    # Get layout json, rm unnecessary comments, uncomment rest, rm unneeded keys
+    i3-save-tree --workspace ${output_name[1]} | tail -n +2 | head -n -1 \
+        | fgrep -v -e '// split' -e '// workspace' | sed 's|// ||g' \
+        | jq \
+        > "$FIREFOX_LAYOUT_PREFIX"_md-"${output_name[0]}"_ws-"${output_name[1]}".json
 
     # TODO Use jq to rm any comment not followed by "class": OR "title":
     # TODO remove any type="con" whose "swallows": [{"class":} is not "firefox$"
-
-    # TODO split output jsons into workspace jsons
 done
-
-# Per workspace:
-
-# Save the i3 layout tree
-i3-save-tree --workspace $WORKSPACE_ID > \
-    "$FIREFOX_LAYOUT_PREFIX"_md-"$MONITOR_ID"_ws-"$WORKSPACE_ID".json
