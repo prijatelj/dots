9e76303f0e41bd2056a50f036f3dd3f56b6e5453
prijatelj <theomnipotentwoof@gmail.com>
2021-08-05 20:22:40 -0400

    saving the jsons as lists of objects.

--- .config/rice/i3/firefox_save.sh
+++ .config/rice/i3/firefox_save.sh
@@ -9,32 +9,28 @@
 # saved layouts
 FIREFOX_LAYOUT_DIR="$HOME/.config/rice/i3/firefox_layouts/"
 if ! ls "$FIREFOX_LAYOUT_DIR" ; then
-    echo "No such file or directory for FIREFOX_LAYOUT_DIR = $FIREFOX_LAYOUT_DIR"
+    printf "No such file or directory for FIREFOX_LAYOUT_DIR = %s\n" \
+        "$FIREFOX_LAYOUT_DIR"
     exit 1
 fi
 
-# Should safely remove any old layout files in the directory before saving
+# TODO Should safely remove any old layout files in the directory before saving
 
 # Loop through the workspaces, saving their layouts if they contain firefox.
 for workspace in $(i3-msg -t get_workspaces | jq -c '.[]'); do
     # Get Array of Output and Name ids.
-    output_name=($(echo $workspace | jq -r '[.output, .name]' | tr -d '[]," '))
+    output_name=($(printf "%s\n" $workspace \
+        | jq -r '[.output, .name]' | tr -d '[]," '))
 
     # Get layout json
-    WS_TREE=$(i3-save-tree --workspace ${output_name[1]})
-
-    echo $WS_TREE \
-        | tail -n +2 | head -n -1
-    #    | fgrep -v -e '// split' -e '// workspace' | sed 's|// ||g'
-    #    | jq -s 'map(.swallows[0] |= {class, title})'
-
-    # If no firefox window in this workspace, continue
-    [[ ! $WS_TREE =~ '^firefox$' ]] && continue
-
-    # rm unnecessary comments, uncomment rest, rm unwanted keys
-    echo $WS_TREE \
-        | tail -n +2 | head -n -1 \
+    # rm unnecessary comments, uncomment rest
+    WS_TREE=$(printf "%s\n" "$(i3-save-tree --workspace ${output_name[1]})" \
+        | tail -n +2 \
         | fgrep -v -e '// split' -e '// workspace' | sed 's|// ||g' \
+    )
+
+    # If no firefox window in this workspace, continue rm unwanted keys & save
+    [[ $WS_TREE  =~ '^firefox$' ]] && printf "%s\n" "$WS_TREE" \
         | jq -s 'map(.swallows[0] |= {class, title})' \
         > "$FIREFOX_LAYOUT_DIR/layout_md-${output_name[0]}_ws-${output_name[1]}.json"
 done
