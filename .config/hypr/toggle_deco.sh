41e2a7f09d87f582a189daa01f87202fd3e89435
prijatelj <derek.s.prijatelj@gmail.com>
2025-02-23 14:30:06 -0800

    tabs, and toggling alacritty opacity.

--- .config/hypr/toggle_deco.sh
+++ .config/hypr/toggle_deco.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
-    hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0;\
-        keyword decoration:active_opacity 1.0;\
-        keyword decoration:inactive_opacity 1.0;"
-    exit
+	sed -i -- "s/opacity =.*$/opacity = 1.0/" ~/.config/alacritty/alacritty.toml
+
+	hyprctl --batch "\
+		keyword animations:enabled 0;\
+		keyword decoration:shadow:enabled 0;\
+		keyword decoration:blur:enabled 0;\
+		keyword general:gaps_in 0;\
+		keyword general:gaps_out 0;\
+		keyword general:border_size 1;\
+		keyword decoration:rounding 0;\
+		keyword decoration:active_opacity 1.0;\
+		keyword decoration:inactive_opacity 1.0;"
+	exit
+else
+	sed -i -- "s/opacity =.*$/opacity = 0.9/" ~/.config/alacritty/alacritty.toml
 fi
 hyprctl reload
